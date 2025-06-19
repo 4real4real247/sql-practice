bc017ddc1d6f8ec75c81a927256a8cef506a4a36
4real4real247 <cassy.h702@gmail.com>
2025-06-19 02:14:55 -0700

    Update murder-mystery-game.sql
    adding more

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -73,6 +73,48 @@ there is 9 tables
   ssn CHAR REFERENCES income (ssn), 
   FOREIGN KEY (license_id) 
   REFERENCES drivers_license (id))
+/////////////////////////////////////////
+SELECT description
+FROM crime_scene_report
+WHERE date = '20180115' AND city = 'SQL City' AND type = 'murder'
+--description
+--Security footage shows that there were 2 witnesses. 
+--The first witness lives at the last house on "Northwestern Dr". 
+--The second witness, named Annabel, lives somewhere on "Franklin Ave".
+
+SELECT *
+FROM person
+WHERE name LIKE 'Annabel%' AND address_street_name = 'Franklin Ave';
+--RESULT
+--id	name	license_id	address_number	address_street_name	ssn
+--*****16371	Annabel Miller	490173	103	Franklin Ave	318771143********
+
+SELECT *
+FROM person
+WHERE address_street_name = 'Northwestern Dr'
+ORDER BY address_number DESC
+LIMIT 3
+--RESULT
+--id	    name	            license_id	address_number	 address_street_name	   ssn
+--***14887	 Morty Schapiro	  118009	  4919	          Northwestern Dr	    111564949*****
+--17729	Lasonya Wildey	   439686	   3824	         Northwestern Dr	    917817122
+--53890	Sophie Tiberio	   957671	   3755         	Northwestern Dr	    442830147
+
+ SELECT transcript
+FROM interview
+WHERE person_id = '16371'
+--RESULTS
+--I saw the murder happen, and I recognized the killer from my gym when I was working 
+-- out last week on January the 9th.
+
+SELECT transcript
+FROM interview
+WHERE person_id = '14887'
+--RESULTS
+--I heard a gunshot and then saw a man run out. He had a "Get Fit Now Gym" bag. The membership number 
+-- on the bag started with "48Z". Only gold members have those bags. The man got into a car with a 
+-- plate that included "H42W".
+
 
 
 -- Record your SQL detective process here!  Write down: 
